ccee928f2f863bdfc1be6d5ef562ea6127be80b1
La Capsule <79408224+lacapsule@users.noreply.github.com>
2021-11-28 18:24:13 +0100

    initialisation

---
+++ osout.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+printf "\e[8;25;40t"
+OS=$(lsb_release -i | cut -d ":" -f 2)
+echo -e "$(tput setaf 0)$(tput setab 9)              Vous utilisez             \n$(tput sgr0)
+            $(tput setaf 0)$(tput setab 3)$OS$(tput sgr0)"
+echo ""
+read -p ' Cela est-il correct ? oui/non ⇾ ' yn
+case $yn in
+        [Yy]* ) echo "$(tput setaf 0)$(tput setab 3)   Vous confirmez utiliser $OS !   "; break;;
+        [Nn]* ) echo ""; 
+        echo "$(tput setaf 0)$(tput setab 3)   Vous devriez utiliser Linux mint !   ";
+        echo ""; 
+        sleep 5; echo "$(tput setaf 7)$(tput setab 0)                AU REVOIR               "; sleep 5;exit;;
+        * ) echo "" 
+        echo "$(tput setaf 0)$(tput setab 9)            Continuons donc...          ";;
+    esac
+sleep 2
+case $OS in
+  'Linux')
+    OS='Linuxmint'
+    ;;
+  'FreeBSD')
+    OS='FreeBSD'
+    alias capsule='cd / ; ~/.lacapsule/./logiciel.sh'
+    ;;
+  'WindowsNT')
+    OS='Windows'
+    ;;
+  'Darwin') 
+    OS='Mac'
+    ;;
+  'SunOS')
+    OS='Solaris'
+    ;;
+  'AIX') ;;
+  *) ;;
+esac
+    echo "$(tput setaf 0)$(tput setab 9)     Nous chargeons l'utilitaire de     $(tput sgr0)"
+    echo "         $(tput setaf 0)$(tput setab 3)$OS$(tput sgr0)"
+    sleep 3
+    echo "$(tput setaf 0)$(tput setab 2)            APT opérationnel            $(tput sgr0)"
+    sleep 1
+    echo "$(tput setaf 0)$(tput setab 1) Nous vérifions l'emplacement du script $(tput sgr0)"
+    sleep 2
+
+    checkfile=/home/$USER/lacapsule
+checkdir() {
+	if [ -e $checkfile ]; then
+		echo "$(tput setaf 0)$(tput setab 2)       Le script est bien présent.      $(tput sgr0)"
+		echo ""
+    echo "                 👍👍👍"
+    echo ""
+		sleep 1.5
+		echo "$(tput setaf 0)$(tput setab 9)   Celui-ci se trouve au bon endroit    $(tput sgr0)"
+		sleep 1.5 
+		echo ""
+		echo "$(tput setaf 0)$(tput setab 7)    Vous le trouverez à cet endroit     $(tput sgr0)"
+		sleep 1.5 
+		echo ""
+		echo "          $checkfile        "
+		echo ""
+    sleep 1.5
+		echo "$(tput setaf 0)$(tput setab 9) Nous allons lancer le script pour vous $(tput sgr0)"
+		sleep 1.5
+    echo "$(tput setaf 0)$(tput setab 9) Obtention des droits sur les fichiers  $(tput sgr0)"
+		sleep 1.5
+		echo ""  
+    cd /home/$USER/ && sudo chmod +x -R lacapsule
+		echo "$(tput setaf 0)$(tput setab 9)  Veuillez entrer votre mot de passe :  $(tput sgr0)"
+    echo "" 
+    /home/$USER/lacapsule/./logiciel.sh
+    sleep 1.5
+	else
+		echo -e "$(tput setaf 0)$(tput setab 9)    Le répertoire n'a pas été trouvé    $(tput sgr0)"
+		sleep 3
+		echo ""
+    echo "$(tput setaf 0)$(tput setab 9)   Nous allons télécharger le script    $(tput sgr0)"
+		sleep 0.5
+    echo ""
+    echo "$(tput setaf 0)$(tput setab 9)  Veuillez entrer votre mot de passe :  $(tput sgr0)"
+    echo "" 
+    cd /home/$USER && apt update;  apt install git; git clone https://github.com/lacapsule/lacapsule.git
+    fi
+
+checkdir
+file=/home/$USER/lacapsule
+checkdir() 
+if [ -f $file ]; then 
+    echo ""
+    echo "$(tput setaf 0)$(tput setab 9)  Le téléchargement du dépôt à réussi   $(tput sgr0)"
+		sleep 3
+    echo "$(tput setaf 0)$(tput setab 9)     Nous allons executer le script       $(tput sgr0)"
+		sleep 1.5
+    echo ""
+    echo "$(tput setaf 0)$(tput setab 9) Obtention des droits sur les fichiers  $(tput sgr0)"
+		sleep 1.5
+		echo ""  
+    cd /home/$USER/ && chmod +x -R lacapsule
+    /home/$USER/lacapsule/./logiciel.sh
+    sleep 3
+else 
+    echo ""
+    echo "$(tput setaf 0)$(tput setab 9)  Le téléchargement du dépôt à échoué   $(tput sgr0)"
+		sleep 3
+    echo "$(tput setaf 0)$(tput setab 9) Vérifiez votre connexion et réessayez  $(tput sgr0)"
+		sleep 5
+    echo ""
+	fi
+}
+checkdir
+exit
